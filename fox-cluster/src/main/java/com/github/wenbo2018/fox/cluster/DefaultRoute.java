2a03a33d6c7a98964a74d663fff56f5213cce787
shenwenbo <wenbo.shen@wenboshendeMacBook-Pro.local>
2017-12-27 22:34:50 +0800

    fix bug

--- fox-cluster/src/main/java/com/github/wenbo2018/fox/cluster/DefaultRoute.java
+++ fox-cluster/src/main/java/com/github/wenbo2018/fox/cluster/DefaultRoute.java
@@ -15,27 +15,28 @@ public class DefaultRoute implements Route {
 
     @Override
     public Client route(List<Client> clients, InvokerConfig invokerConfig) {
-        LoadBalance loadBalance=null;
+        LoadBalance loadBalance = null;
         if (clients != null && clients.size() > 0) {
-            String loadBalanceType=invokerConfig.getLoadBalance();
-            if (loadBalanceType!=null) {
-                loadBalance= ExtensionServiceLoader.getExtensionLoader(LoadBalance.class)
+            String loadBalanceType = invokerConfig.getLoadBalance();
+            if (loadBalanceType != null) {
+                loadBalance = ExtensionServiceLoader.getExtensionLoader(LoadBalance.class)
                         .getExtension(resolveLoadBalance(loadBalanceType));
             } else {
-                loadBalance= ExtensionServiceLoader.getExtensionLoader(LoadBalance.class)
+                loadBalance = ExtensionServiceLoader.getExtensionLoader(LoadBalance.class)
                         .getExtension(ClusterConstants.DEFAULT_LOADBALANCE);
             }
         }
-        return loadBalance.select(clients,invokerConfig);
+        return loadBalance.select(clients, invokerConfig);
     }
 
-    private String resolveLoadBalance(String loadBalanceType){
+    private String resolveLoadBalance(String loadBalanceType) {
         switch (loadBalanceType) {
             case ClusterConstants.RANDOM_LOADBALANCE_CONFIG:
                 return ClusterConstants.RANDOM_LOADBALANCE;
             case ClusterConstants.LEASTACTIVELOADBALANCE_LOADBALANCE_CONFIG:
                 return ClusterConstants.LEASTACTIVELOADBALANCE_LOADBALANCE;
-            default:return ClusterConstants.DEFAULT_LOADBALANCE;
+            default:
+                return ClusterConstants.DEFAULT_LOADBALANCE;
         }
     }
 
